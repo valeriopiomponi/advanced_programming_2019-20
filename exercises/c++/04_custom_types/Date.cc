d787254c415c7661ca7c5ad4267208b26cd5cf9b
Valerio Piomponi <valerio.piomponi@gmail.com>
2019-10-31 17:04:38 +0100

    Completed program for a year of 367 days (febraury you are not so special)

--- exercises/c++/04_custom_types/Date.cc
+++ exercises/c++/04_custom_types/Date.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-enum class month{jan, feb, mar, apr, may, jui, jul, aug, sep, oct, nov, dec};
+enum class month{jan=1, feb, mar, apr, may, jui, jul, aug, sep, oct, nov, dec};
 
 class date {
 
@@ -18,17 +18,17 @@ class date {
   {}
  
   
-  int day(const int _d) {
+  int day() {
 
     return _d;
   }
 
-  month fmonth(const month _m) {
+  month fmonth() {
 
     return _m;
   }
 
-  int year(const int _y) {
+  int year() {
 
     return _y;
   }
@@ -47,27 +47,37 @@ class date {
     
 
     switch(_m){
-    case _m==month::jan: case _m==month::mar: case _m==month::may: case _m==month::jul: case _m==month::aug: case _m==month::oct: case _m==month::dec:
-      if(d < 31){
+    case month::jan: case month::mar: case month::may: case month::jul: case month::aug: case month::oct: case month::dec:
+      if(_d < 31){
 	_d += 1;
       }
 
-      else if(_m =! month::dec) {
+      else if(_m != month::dec) {
 	_d = 1;
-	_m = _m(int(_m) +1));
+	_m = month(int(_m) +1);
       }
 
       else {
 	_d = 1;
 	_m = month::jan;
-	_y =+ 1;
+	_y += 1;
       }
-    }
-	
-	  
     
+      break;
 
-    
+    case month::feb: case month::apr: case month::jui: case month::sep: case month::nov:
+      if(_d < 30){
+       _d += 1;
+      }
+
+      else {
+	_d = 1;
+	_m = month(int(_m)+1);
+      }
+
+      break;
+    }
+  }
     
 
 };
@@ -79,10 +89,10 @@ int main(){
 
   month mon{month::jan};
 
-  date pd{5, mon, 2000};
+  date pd{1, mon, 2000};
 
-  pd.add_days(5);
+  pd.add_days(367);
 
-  cout<< pd._d << endl;
+  cout << pd.day()<< "-" << int(pd.fmonth())<< "-"<< pd.year() << endl;
 
 }
