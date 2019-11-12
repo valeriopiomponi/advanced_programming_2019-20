4e9a19e6345a8ea0c27b192edba29621d2bf613d
valeriopiomponi <valerio.piomponi@gmail.com>
2019-11-12 17:35:49 +0100

    final LinkedList

--- exercises/c++/05_copy_move/LinkList.cc
+++ exercises/c++/05_copy_move/LinkList.cc
@@ -3,20 +3,35 @@
 #include<algorithm>
 #include<utility>
 
+#include "ap_error.h"
+
 
 enum class Insertion_method { push_back, push_front };
 
 template <class value_type>
 class List {
  public:
+  List() noexcept = default;
+  List(List&& l) noexcept = default;
+  List& operator=(List&& l) noexcept = default;
+
+  List(const List& l);
+  List& operator=(const List& l);
+  
   // insert a new node with the value v according to the method m
   // this method should be used to fill the list
-  void insert(value_type&& v, const Insertion_method m);
-  void insert(const value_type& v, const Insertion_method m){
+  template <class OT>
+  void insert(OT&& v, const Insertion_method m);
+
+
+  template <class O>
+  friend std::ostream& operator<<(std::ostream&, const List<O>&);
+  
+  /*void insert(const value_type& v, const Insertion_method m){
 
 	if(int(m)==0) {push_back(v);}
 	else if(int(m)==1) {push_front(v);}
-  }
+	}*/
 
   // return the size of the list
   std::size_t sizelist() const;
@@ -24,16 +39,6 @@ class List {
   // delete all the nodes of the list
   void reset();
 
-  // constructor(s) for List
-  List() noexcept = default;
-  List(List&&) noexcept = default;
-  // copy semantics
-  
-  List(cost List& 1);
-  List& operator=(const List&& 1);
-  // move semantics
-  
-  // destructor
 
  private:
  
@@ -44,10 +49,10 @@ class List {
 
     // implement suitable constructor(s) for node
     node(const value_type& v, node* p) : next{p}, value{v} {
-    std:cout << "copy ctor" << std::endl;
+      std::cout << "copy ctor" << std::endl;
     }
 
-    node(type_value&& v, node* p) : next{p}, value{std::move(v)} {
+    node(value_type&& v, node* p) : next{p}, value{std::move(v)} {
       std::cout << "move ctor" << std::endl;
     }
 
@@ -105,7 +110,7 @@ class List {
           head = std::make_unique<node>(v,head,release());
 	  }*/
 
-  node* tail(){
+  node* tail() noexcept{
 	auto tmp = head.get();
 	while(tmp->next){
 		tmp=tmp->next.get();
@@ -122,27 +127,35 @@ class List {
 
 
 template <class T>
-void List<T>::insert(const T& v, const method m) {
+template <class OT>
+void List<T>::insert(OT&& v, const Insertion_method m) {
 	if(!head){
-		head.reset(new node{v,nullptr});
+	  //head.reset(new node{v,nullptr});
+	  head = std::make_unique<node>(std::forward<OT>(v), nullptr);
 		return;
 	}
-	switch(m){
-	case Insertion_method::push_back
- 	 push_back(v);
-	 break;
 	
-	case Insertion_method::push_front
-	 push_front(v);
-	 break;
+	switch(m){
+	case Insertion_method::push_back:
+	   push_back(std::forward<OT>(v));
+	   break;
+	
+	case Insertion_method::push_front:
+	   push_front(std::forward<OT>(v));
+	   break;
+
+	default:
+	  AP_ERROR(false) << "Unknown insertion method";
+	  break;
 	};
 }
 
 
-template <class T>
-void List<T>::push_back(const T& v){
+/*template <class T>
+tepmlate <class OT>
+void List<T>::push_back(OT&& v){
 	node* last=tail();
-	last->next =std::make_unique<node>{v,nullptr};
+	last->next =std::make_unique<node>{std::forward<OT>(v),nullptr};*/
 
 template <class T>
 std::ostream& operator<<(std::ostream& os, const List<T>& l){
@@ -154,16 +167,36 @@ std::ostream& operator<<(std::ostream& os, const List<T>& l){
 	return os;
 }
 
-
-
-
-int main(
-{
-	try{
-	}catch(std::exception& e){
-		std::cerr<<e.what()<<std::endl;
-	}catch(...){
-		std::cerr<<"unknown exception"<< std::endl;
-	}
-
+template <class T>
+List<T>::List(const List& l) {
+  head = std::make_unique<node>(l.head);
 }
+
+
+
+int main() {
+	try{
+	  List<int> l{};
+
+	  l.insert(4, Insertion_method::push_back);
+	  l.insert(5, Insertion_method::push_back);
+	  l.insert(3, Insertion_method::push_front);
+
+	  std::cout<<l<<std::endl;
+
+	  auto ol = l;
+	  int a = 9;
+	  l.insert(14, Insertion_method::push_front);
+	  l.insert(a, Insertion_method::push_front);
+
+	  std::cout << l << std::endl;
+	  std::cout << ol << std::endl;
+
+	} catch (std::exception& e) {
+	  std::cerr << e.what() << std::endl;
+	
+	} catch(...) {
+	  std::cerr << "Unknown exception" << std::endl;
+	}
+	return 0;
+ }
